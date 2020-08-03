dbac9fc4abf06f1063f584135e4aeaa3f26314e9
Gavin Raine Dizon <60138634+gavindizon@users.noreply.github.com>
2020-08-03 12:51:32 +0800

    Added pages

---
+++ src/components/about/team.js
@@ -0,0 +1,44 @@
+import React from "react";
+import members from "../../data/members.json";
+import { Container, Row, Col } from "reactstrap";
+
+const ListTeam = ({ teamMembers }) => {
+  const list = teamMembers.map((member, index) => {
+    const social_list = member.social_media.map((social) => {
+      return social;
+    });
+    return (
+      <div
+        md={3}
+        xs={6}
+        className="member-card margin-bottom-64 text-center"
+        key={index}
+      >
+        <div className="avatar-placeholder"></div>
+        <h5>{member.name}</h5>
+        <p>{member.position}</p>
+        <div className="social-links">{/*{social_list}*/}</div>
+      </div>
+    );
+  });
+
+  return <div class="team-row margin-x-128">{list}</div>;
+};
+
+const Team = () => {
+  return (
+    <section className="padding-y-128 team-section">
+      <Container>
+        <h2 className="margin-bottom-96 text-center">Our Organizing Team</h2>
+        <h3 className="margin-bottom-32 text-center">Team Leads</h3>
+        <ListTeam teamMembers={members.committee_leads} />
+        <h3 className="margin-bottom-32 text-center">Core Team</h3>
+        <ListTeam teamMembers={members.coordinators} />
+        <h3 className="margin-bottom-32 text-center">Advisory Board</h3>
+        <ListTeam teamMembers={members.board_members} />
+      </Container>
+    </section>
+  );
+};
+
+export default Team;
